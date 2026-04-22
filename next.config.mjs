6078aab03991a3f8dfe6004da4b9eebc144685e3
Gunit Agarwal <gunitagarwal163@gmail.com>
2026-04-22 22:26:11 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/GunitVerse",
+  assetPrefix: "/GunitVerse/",
+
   typescript: {
     ignoreBuildErrors: true,
   },
