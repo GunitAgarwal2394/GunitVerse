a28e6b551d073db7b87ae5f816058e3e1abae2ae
Gunit Agarwal <gunitagarwal163@gmail.com>
2026-04-22 23:05:22 +0530

    fix image path

---
+++ out/_next/static/mlYepEv6BJJ1ncyzqKecd/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/GunitVerse//_next/:path+",
+        "destination": "/GunitVerse/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
